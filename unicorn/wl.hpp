cb1bd531c3556e30b22cd803ad44180abb51c22f
Sebastian Kvalden <sebastian.kvalden@gmai.com>
2025-02-28 12:30:01 +0000

    added prints

--- unicorn/wl.hpp
+++ unicorn/wl.hpp
@@ -492,7 +492,7 @@ namespace graphchi {
 				hist->decay(SFP);  /* Only decay once. */
 				first = false;
 			    }
-				//std::string rootString = rootToString(vertex.id(), vertex.get_data().roots);
+				std::string rootString = rootToString(vertex.id(), vertex.get_data().roots);
 				//unsigned long rootHash = hash((unsigned char *)rootString.c_str());
 				//unsigned long rootHash = rootEmbedding(vertex.get_data().roots);
 			    hist->update(*ti, false);
