d870a0bfbbd27aaa6056b9bfb6b50af36c87f350
Sebastian Kvalden <sebastian.kvalden@gmail.com>
2025-03-04 15:24:11 +0100

    removed changes

--- unicorn/histogram.cpp
+++ unicorn/histogram.cpp
@@ -89,13 +89,13 @@ void Histogram::decay(FILE* fp) {
 /* Insert @label to the histogram if it does not exist; otherwise, update its value.
  * If @base true, we do not update hash value; we only update them during streaming.
  * We do not decay the histogram or the sketch in this function. */
-void Histogram::update(unsigned long label, bool base, double counter = 1) {
+void Histogram::update(unsigned long label, bool base) {
 
     this->histogram_map_lock.lock();
     /* We add the new element or update the existing element in the
      * histogram. This is done both in base and stream graph. */
     std::pair<std::map<unsigned long, double>::iterator, bool> rst;
-    //double counter = 1;
+    double counter = 1;
     rst = this->histogram_map.insert(std::pair<unsigned long, double>(label, counter));
     if (rst.second == false) {
 #ifdef DEBUG
