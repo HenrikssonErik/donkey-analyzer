4b84fd5545a1f5ce9b246ba4c8d122c0b7dd2035
Sebastian Kvalden <sebastian.kvalden@gmail.com>
2025-03-25 13:48:48 +0100

    removed prints

--- unicorn/main.cpp
+++ unicorn/main.cpp
@@ -385,9 +385,9 @@ int main(int argc, const char ** argv) {
         return -1;
     }
 
-    logstream(LOG_INFO) << root_handler->printSketch() << std::endl;
+    //logstream(LOG_INFO) << root_handler->printSketch() << std::endl;
     //Print root histogram for debugging
-    logstream(LOG_INFO) << root_handler->printHistogram() << std::endl;
+    //logstream(LOG_INFO) << root_handler->printHistogram() << std::endl;
 
     /* Release the barrier resources. */
     int ret_stream = pthread_barrier_destroy(&std::stream_barrier);
